91bfc3cb68a69057dbd31f643cbeb084b9edf507
Victory James <javik@live.com>
2014-07-23 17:10:21 +0100

    Before CMS inclusion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,63 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).ready(function()
+{
+	function initialize() {
+        var map_canvas = document.getElementById('map_canvas');
+        var myLatlng = new google.maps.LatLng(6.4475657, 3.4724711);
+        var MY_MAPTYPE_ID = 'custom_style';
+
+        var featureOpts = [
+	    {
+	      stylers: [
+	        { hue: '#fdbc40' },
+	        { gamma: 1 },
+	        { weight: 3 }
+	      ]
+	    },
+	    {
+	      featureType: 'water',
+	      stylers: [
+	        { color: '#43bcf0' }
+	      ]
+	    }
+	  ];
+
+        var map_options = {
+          center: myLatlng,
+          zoom: 16,
+           mapTypeControlOptions: {
+		      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+		   },
+		   mapTypeId: MY_MAPTYPE_ID
+        };
+
+        var map = new google.maps.Map(map_canvas, map_options);
+
+        var marker = new google.maps.Marker({
+        	position: myLatlng,
+        	map: map,
+        	title: 'SuperGeeks',
+    		animation: google.maps.Animation.DROP,
+        });
+
+        google.maps.event.addListener(marker, 'click', toggleBounce);
+
+        var styledMapOptions = {
+		    name: 'Custom Style'
+		 };
+
+		 var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+        map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+      };
+
+      function toggleBounce() {
+		  if (marker.getAnimation() != null) {
+		    marker.setAnimation(null);
+		  } else {
+		    marker.setAnimation(google.maps.Animation.BOUNCE);
+		  }
+		}
+      
+
+      initialize();
+});
